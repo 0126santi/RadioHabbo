a3cc1f4a22282054a5a1cc7fd2f9a17f8b603cb1
Santiago Rodriguez <Srodriguezl.2601@gmail.com>
2025-06-27 18:34:00 -0400

    reestructuracion con next.js

--- api/hora.js
+++
@@ -1,9 +0,0 @@
-import { DateTime } from "luxon";
-
-export default function handler(req, res) {
-  const caracas = DateTime.now().setZone("America/Caracas");
-  res.status(200).json({
-    hora: caracas.toFormat("hh:mm a"),
-    fecha: caracas.toFormat("dd 'de' LLLL 'de' yyyy")
-  });
-}
